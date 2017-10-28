7ec18a18ccaaf3c3b086bc2864ef25eef1b15da8
Samuel Holland <samuel@sholland.org>
2017-10-27 23:17:38 -0500

    MainActivity: Add basic layout

--- app/src/main/java/me/tigerapps/foodly/MainActivity.java
+++ app/src/main/java/me/tigerapps/foodly/MainActivity.java
@@ -1,11 +1,13 @@
 package me.tigerapps.foodly;
 
 import android.app.Activity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        DataBindingUtil.setContentView(this, R.layout.main_activity);
     }
 }
