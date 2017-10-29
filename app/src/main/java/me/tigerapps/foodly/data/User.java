1ea1e22805f7bfcba44963e62dffa53b55831036
Samuel Holland <samuel@sholland.org>
2017-10-29 05:02:18 -0500

    User: fix types, add macro gate

--- app/src/main/java/me/tigerapps/foodly/data/User.java
+++ app/src/main/java/me/tigerapps/foodly/data/User.java
@@ -11,32 +11,35 @@ public class User extends BaseObservable {
     private static final String USER_SEX = "user_sex";
     private static final String USER_HEIGHT = "user_height";
     private static final String USER_WEIGHT = "user_weight";
+    private static final String MACRO_ADVANCED = "macro_advanced";
     private static final String MACRO_FAT = "macro_fat";
     private static final String MACRO_CARBS = "macro_carbs";
     private static final String MACRO_PROTEIN = "macro_protein";
 
     private int age;              // In years
-    private double calories;      // Kilocalories per day
+    private double calories;      // In kilocalories per day
     private double carbs;         // In grams per day
-    private float carbsRatio;     // Percent (0-100)
+    private int carbsRatio;       // Percent (0-100)
     private double fat;           // In grams per day
-    private float fatRatio;       // Percent (0-100)
+    private int fatRatio;         // Percent (0-100)
     private int height;           // In centimeters
+    private boolean isCustomDiet;
     private double protein;       // In grams per day
-    private float proteinRatio;   // Percent (0-100)
+    private int proteinRatio;     // Percent (0-100)
     private Sex sex;
     private int weight;           // In kilograms
     private final SharedPreferences preferences;
 
-    public User(SharedPreferences preferences) {
+    public User(final SharedPreferences preferences) {
         this.preferences = preferences;
         age = preferences.getInt(USER_AGE, 20);
         sex = Sex.valueOf(preferences.getString(USER_SEX, "MALE"));
         height = preferences.getInt(USER_HEIGHT, 150);
+        isCustomDiet = preferences.getBoolean(MACRO_ADVANCED, false);
         weight = preferences.getInt(USER_WEIGHT, 45);
-        fatRatio = preferences.getFloat(MACRO_FAT, 30);
-        carbsRatio = preferences.getFloat(MACRO_CARBS, 40);
-        proteinRatio = preferences.getFloat(MACRO_PROTEIN, 30);
+        fatRatio = preferences.getInt(MACRO_FAT, 30);
+        carbsRatio = preferences.getInt(MACRO_CARBS, 40);
+        proteinRatio = preferences.getInt(MACRO_PROTEIN, 30);
         calculateCalories();
         calculateMacronutrients();
     }
@@ -57,7 +60,7 @@ public class User extends BaseObservable {
     /**
      * Calculates amounts of macronutrients in grams needed to meet the Calorie goal
      */
-    public void calculateMacronutrients() {
+    private void calculateMacronutrients() {
         this.fat = (calories * fatRatio) / 900;
         this.carbs = (calories * carbsRatio) / 400;
         this.protein = (calories * proteinRatio) / 400;
@@ -95,7 +98,7 @@ public class User extends BaseObservable {
     }
 
     @Bindable
-    public float getCarbsRatio() {
+    public int getCarbsRatio() {
         return carbsRatio;
     }
 
@@ -110,7 +113,7 @@ public class User extends BaseObservable {
     }
 
     @Bindable
-    public float getFatRatio() {
+    public int getFatRatio() {
         return fatRatio;
     }
 
@@ -130,7 +133,7 @@ public class User extends BaseObservable {
     }
 
     @Bindable
-    public float getProteinRatio() {
+    public int getProteinRatio() {
         return proteinRatio;
     }
 
@@ -144,6 +147,11 @@ public class User extends BaseObservable {
         return weight;
     }
 
+    @Bindable
+    public boolean isCustomDiet() {
+        return isCustomDiet;
+    }
+
     public void setAge(final int age) {
         this.age = age;
         SharedPreferences.Editor ed = preferences.edit();
@@ -154,16 +162,24 @@ public class User extends BaseObservable {
         calculateMacronutrients();
     }
 
-    public void setCarbsRatio(final float carbsRatio) {
-        final double delta = this.carbsRatio - carbsRatio;
+    public void setCarbsRatio(final int carbsRatio) {
+        final int delta = this.carbsRatio - carbsRatio;
         this.fatRatio += delta / 2;
         this.proteinRatio += delta / 2;
         this.carbsRatio -= delta;
         updateMacronutrients(this.carbsRatio, this.fatRatio, this.proteinRatio);
     }
 
-    public void setFatRatio(final float fatRatio) {
-        final double delta = this.fatRatio - fatRatio;
+    public void setCustomDiet(boolean isCustomDiet) {
+        this.isCustomDiet = isCustomDiet;
+        SharedPreferences.Editor ed = preferences.edit();
+        ed.putBoolean(MACRO_ADVANCED, isCustomDiet);
+        ed.commit();
+        notifyPropertyChanged(BR.customDiet);
+    }
+
+    public void setFatRatio(final int fatRatio) {
+        final int delta = this.fatRatio - fatRatio;
         this.carbsRatio += delta / 2;
         this.proteinRatio += delta / 2;
         this.fatRatio -= delta;
@@ -180,8 +196,8 @@ public class User extends BaseObservable {
         calculateMacronutrients();
     }
 
-    public void setProteinRatio(final float proteinRatio) {
-        final double delta = this.proteinRatio - proteinRatio;
+    public void setProteinRatio(final int proteinRatio) {
+        final int delta = this.proteinRatio - proteinRatio;
         this.carbsRatio += delta / 2;
         this.fatRatio += delta / 2;
         this.proteinRatio -= delta;
@@ -208,14 +224,15 @@ public class User extends BaseObservable {
         calculateMacronutrients();
     }
 
-    private void updateMacronutrients(final float carbsRatio, final float fatRatio, final float proteinRatio) {
+    private void updateMacronutrients(final int carbsRatio, final int fatRatio,
+                                      final int proteinRatio) {
         this.carbsRatio = carbsRatio;
         this.fatRatio = fatRatio;
         this.proteinRatio = proteinRatio;
         SharedPreferences.Editor ed = preferences.edit();
-        ed.putFloat(MACRO_CARBS, carbsRatio);
-        ed.putFloat(MACRO_FAT, fatRatio);
-        ed.putFloat(MACRO_PROTEIN, proteinRatio);
+        ed.putInt(MACRO_CARBS, carbsRatio);
+        ed.putInt(MACRO_FAT, fatRatio);
+        ed.putInt(MACRO_PROTEIN, proteinRatio);
         ed.commit();
         notifyPropertyChanged(BR.carbsRatio);
         notifyPropertyChanged(BR.fatRatio);
