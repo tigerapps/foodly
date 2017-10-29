84e38f02afc2bb5e661338fee7026f53bff4fb38
Samuel Holland <samuel@sholland.org>
2017-10-28 20:21:14 -0500

    utils/Converter: Make parameters final

--- app/src/main/java/me/tigerapps/foodly/utils/Converter.java
+++ app/src/main/java/me/tigerapps/foodly/utils/Converter.java
@@ -1,19 +1,19 @@
 package me.tigerapps.foodly.utils;
 
 public final class Converter {
-    public static double inchesToCm(double inches) {
+    public static double inchesToCm(final double inches) {
         return (inches * 2.54);
     }
 
-    public static double CmToInches(double cm) {
+    public static double CmToInches(final double cm) {
         return (cm / 2.54);
     }
 
-    public static double LbsToKg(double lbs) {
+    public static double LbsToKg(final double lbs) {
         return (lbs / 2.2);
     }
 
-    public static double KgToLbs(double kg) {
+    public static double KgToLbs(final double kg) {
         return (kg * 2.2);
     }
 }
